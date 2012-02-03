e849b99d56306c3f45e7d6d5914b1babbfe9722b
zcutlip <uid000@gmail.com>
2012-02-03 14:29:11 +0000

    added tests target to Makefile. in test_list.c broke out creation & population of list into separate funciton. broke out testing of llremove_last into seperate function. added testing of llremove_first.

--- src/tests/test_list.c
+++ src/tests/test_list.c
@@ -6,23 +6,19 @@
 #include "adt_list.h"
 #include "adt_error.h"
 
-#define DATA_COUNT 100
+#define DATA_COUNT 10000
 
-int main(void)
+LIST setup_and_populate_list(adt_test_data *test_data)
 {
     int i;
-    char *data;
-    adt_status status;
-    adt_test_data *test_data;
-    adt_test_item *t_item;
-    
-    NODE node;
     LIST list;
-    
-    printf("creating test data.\n");
-    test_data=adt_test_data_create(DATA_COUNT);
+    adt_status status;
+    adt_test_item *t_item;
+    NODE node;
 
-    printf("creating list.");
+    assert(NULL!=test_data);
+
+    printf("creating list.\n");
     list=list_create();
     assert(NULL!=list);
     
@@ -36,22 +32,88 @@ int main(void)
         assert(ADT_OK==status);
 
     }
-    
+    return list;
+
+}
+
+
+adt_status test_remove_last(adt_test_data *test_data)
+{
+    int i;
+    char *data;
+    adt_status status;
+    adt_test_item *t_item;
+
+    LIST list;
+
+    list=setup_and_populate_list(test_data);
+    data=NULL;
     printf("removing data from list.\n");
-    for(i=0;i<DATA_COUNT;i++)
+    for(i=DATA_COUNT-1;i>=0;i--)
     {
         t_item=test_data->test_items[i];
         status=llremove_last(list,(void **)(&data));
         assert(ADT_OK==status);
         assert(strncmp(t_item->data,data,ADT_TEST_MAX_STRLEN)==0);
-
+        data=NULL;
     }
 
     printf("destroying list.\n");
     list_destroy(&list);
 
     printf("Destroyed list.\n");
+
+    status=ADT_OK;
     
+    return status;
+}
+
+adt_status test_remove_first(adt_test_data *test_data)
+{
+    int i;
+    char *data;
+    adt_status status;
+    adt_test_item *t_item;
+    
+    LIST list;
+
+    list=setup_and_populate_list(test_data);
+    assert(NULL != list);
+    data=NULL;
+    printf("removing data from list.\n");
+    for(i=0;i<DATA_COUNT;i++)
+    {
+        t_item=test_data->test_items[i];
+        status=llremove_first(list,(void **)(&data));
+        assert(ADT_OK==status);
+        assert(strncmp(t_item->data,data,ADT_TEST_MAX_STRLEN)==0);
+        data=NULL;
+    }
+
+    printf("destroying list.\n");
+    list_destroy(&list);
+    printf("Destroyed list.\n");
+
+    status=ADT_OK;
+    
+    return status;
+}
+
+int main(void)
+{
+    adt_status status;
+    adt_test_data *test_data;
+    
+    
+    printf("creating test data.\n");
+    test_data=adt_test_data_create(DATA_COUNT);
+    
+    status=test_remove_last(test_data);
+    assert(ADT_OK==status);
+    
+    status=test_remove_first(test_data);
+    assert(ADT_OK==status);
+
     printf("destroying test data.\n");
     adt_test_data_destroy(&test_data);
 
