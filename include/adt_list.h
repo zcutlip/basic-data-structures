00878d59221ab187442c70551d079c835d5597fc
zcutlip <uid000@gmail.com>
2012-04-09 13:53:54 +0000

    added llinsert_before and llinsert_after to header

--- include/adt_list.h
+++ include/adt_list.h
@@ -10,11 +10,14 @@ LIST list_create(void);
 void list_destroy(LIST *list_p);
 
 adt_status lladd(LIST list, void *data, int key,NODE* node_p);
+adt_status llinsert_after(LIST list, void *data, int key, NODE *node_p);
+adt_status llinsert_before(LIST list, void *data, int key, NODE *node_p);
 
 adt_status llget_first(LIST list, NODE *node_p, void **data_p);
 adt_status llget_last(LIST list, NODE *node_p, void **data_p);
 adt_status llget_prev(LIST list, NODE *node_p, void **data_p);
 adt_status llget_next(LIST list, NODE *node_p, void **data_p);
+
 
 /* get a handle on the list's "current" node, and get it's data */
 adt_status llget_current(LIST list, NODE *node_p, void **data_p);
@@ -24,3 +27,4 @@ adt_status llremove_first(LIST list, void **data_p);
 adt_status llremove_last(LIST list, void **data_p);
 
 #endif /* _ADT_LIST_H */
+
