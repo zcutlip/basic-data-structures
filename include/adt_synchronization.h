19f2e919ce3eaa99a631c0502b5539d1a540ad26
Zachary Cutlip <uid000@gmail.com>
2014-12-16 11:30:41 -0800

    moved posix declarations to a separate header file

--- include/adt_synchronization.h
+++ include/adt_synchronization.h
@@ -15,23 +15,17 @@ typedef struct adt_lock_t *ADT_LOCK;
 #define _adt_write_lock(x)      _adt_xnu_write_lock(x)
 #define _adt_write_unlock(x)    _adt_xnu_write_unlock(x)
 #define _adt_rw_lock_destroy(x) _adt_xnu_rw_lock_destroy(x)
+
 #else
 
-#define ADT_POSIX_SYNC
+#include "adt_posix_synchronizaton.h"
 
 #define _adt_rw_lock_init()    _adt_posix_rw_lock_init()
-#define _adt_read_lock(x)       _adt_poxix_read_lock(x)
+#define _adt_read_lock(x)       _adt_posix_read_lock(x)
 #define _adt_read_unlock(x)     _adt_posix_read_unlock(x)
 #define _adt_write_lock(x)      _adt_posix_write_lock(x)
 #define _adt_write_unlock(x)    _adt_posix_write_unlock(x)
 #define _adt_rw_lock_destroy(x) _adt_posix_rw_lock_destroy(x)
-
-ADT_LOCK _adt_posix_rw_lock_init();
-adt_status _adt_posix_read_lock(ADT_LOCK lock);
-adt_status _adt_posix_read_unlock(ADT_LOCK lock);
-adt_status _adt_posix_write_lock(ADT_LOCK lock);
-adt_status _adt_posix_write_unlock(ADT_LOCK lock);
-void _adt_xnu_posix_lock_destroy(ADT_LOCK *lockp);
 
 #endif
 
